d34f55a976a0ca1bf0dfc8ae5230dc6d4523b1f6
atarazon@terpmail.umd.edu <atarazona1997@gmail.com>
2022-12-02 12:18:47 -0500

    Lab 8 changes for map

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -12,11 +12,11 @@
 */
 
 function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  const newmin = Math.ceil(min);
+  const newmax = Math.floor(max);
+  return Math.floor(Math.random() * (newmax - newmin + 1) + newmin);
 }
-    
+
 function injectHTML(list) {
   console.log('fired injectHTML');
   const target = document.querySelector('#restaurant_list');
@@ -45,7 +45,7 @@ function injectHTML(list) {
         - Display the name of that restaurant and what category of food it is
     */
 }
-    
+
 function processRestaurants(list) {
   console.log('fired restaurants list');
   const range = [...Array(15).keys()]; // Special notation to create and array of 15 elements
@@ -53,25 +53,10 @@ function processRestaurants(list) {
     const index = getRandomIntInclusive(0, list.length);
     return list[index];
   });
+
   return newArray;
-    
-  /*
-        ## Process Data Separately From Injecting It
-          This function should accept your 1,000 records
-          then select 15 random records
-          and return an object containing only the restaurant's name, category, and geocoded location
-          So we can inject them using the HTML injection function
-          You can find the column names by carefully looking at your single returned record
-          https://data.princegeorgescountymd.gov/Health/Food-Inspection/umjn-t2iz
-    
-        ## What to do in this function:
-    
-        - Make a list of 15 random restaurants from your list of 100 from your data request
-        - Return only their name, category, and location
-        - Return the new list of 15 restaurants so we can work on it separately in the HTML injector
-      */
 }
-  
+
 function filterList(list, filterInputValue) {
   return list.filter((item) => {
     if (!item.name) { return; }
@@ -80,7 +65,34 @@ function filterList(list, filterInputValue) {
     return lowerCaseName.includes(lowerCaseQuery);
   });
 }
-    
+
+function initMap() {
+  // so much so familiar, but we will need this to inject markers later!
+  console.log('initMap');
+  const map = L.map('map').setView([38.9897, -76.9378], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+  return map;
+}
+
+function markerPlace(array, map) {
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Marker) {
+      layer.remove();
+    }
+  });
+
+  array.forEach((item, index) => {
+    const {coordinates} = item.geocoded_column_1;
+    L.marker([coordinates[1], coordinates[0]]).addTo(map);
+    if (index === 0) {
+      map.setView([coordinates[1], coordinates[0]], 10);
+    }
+  });
+}
+
 async function mainEvent() {
   /*
         ## Main Event
@@ -88,13 +100,14 @@ async function mainEvent() {
           When you're not working in a heavily-commented "learning" file, this also is more legible
           If you separate your work, when one piece is complete, you can save it and trust it
       */
-    
+  const pageMap = initMap();
+
   // the async keyword means we can make API requests
   const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
   const submit = document.querySelector('#get-resto'); // get a reference to your submit button
   const loadAnimation = document.querySelector('.lds-ellipsis'); // get a reference to our loading animation
   submit.style.display = 'none'; // let your submit button disappear
-    
+
   /*
         Let's get some data from the API - it will take a second or two to load
         This next line goes to the request for 'GET' in the file at /server/routes/foodServiceRoutes.js
@@ -109,9 +122,9 @@ async function mainEvent() {
         Dot notation is preferred in JS unless you have a good reason to use brackets
         The 'data' key, which we set at line 38 in foodServiceRoutes.js, contains all 1,000 records we need
       */
-  // console.log('Test PG County Set');
-  // console.table(arrayFromJson.data);
-    
+  console.log('Test PG County Set');
+  console.table(arrayFromJson.data);
+
   // in your browser console, try expanding this object to see what fields are available to work with
   // for example: arrayFromJson.data[0].name, etc
   console.log(arrayFromJson.data[0]);
@@ -133,6 +146,7 @@ async function mainEvent() {
       console.log(event.target.value);
       const filteredList = filterList(currentList, event.target.value);
       injectHTML(filteredList);
+      marketPlace(filteredList, pageMap);
     });
   
     // And here's an eventListener! It's listening for a "submit" button specifically being clicked
@@ -146,6 +160,7 @@ async function mainEvent() {
     
       // And this function call will perform the "side effect" of injecting the HTML list for you
       injectHTML(currentList);
+      marketPlace(currentList, pageMap);
     
       // By separating the functions, we open the possibility of regenerating the list
       // without having to retrieve fresh data every time
